8d752ef43d2ead28dcfa9a79f0401e55de26a480
David Freifeld <freifeld.david@gmail.com>
2020-07-05 17:58:18 -0700

    Beginnings of forces, 3 dimensions will complicate things

--- matter.cpp
+++ matter.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <array>
 #include <iostream>
+#include <cmath>
 #include <Eigen/Dense>
 
 #define GRAV_CONST (6.674 * pow(10,-11))
@@ -14,6 +15,22 @@ public:
   Eigen::Vector3d acceleration;
   Matter(double m, std::array<double, 3> x, std::array<double, 3> v, std::array<double, 3> a);
 };
+
+struct Force
+{
+public:
+  Eigen::Vector3d components;
+  Matter* target;
+  Matter* source;
+  Force(Matter* end, Matter* src, std::array<double, 3> parts);
+};
+
+Force::Force(Matter* end, Matter* src, std::array<double, 3> parts)
+{
+  for (int i = 0; i < 3; i++) {
+    components[i] = parts[i];
+  }
+}
 
 Matter::Matter(double m, std::array<double, 3> x, std::array<double, 3> v, std::array<double, 3> a)
 {
@@ -32,6 +49,7 @@ Matter::Matter(double m, std::array<double, 3> x, std::array<double, 3> v, std::
 class Universe
 {
   std::vector<Matter> matter;
+  std::vector<Force> forces;
   void update_matter(Matter* obj);
 public:
   void add_matter(double m, std::array<double, 3> x, std::array<double, 3> v, std::array<double, 3> a);
@@ -47,6 +65,8 @@ Universe::Universe()
 void Universe::add_matter(double m, std::array<double, 3> x, std::array<double, 3> v, std::array<double, 3> a)
 {
   matter.emplace_back(m,x,v,a);
+  
+  forces.emplace_back()
   // TODO: Force stuff
 }
 
@@ -62,7 +82,16 @@ void Universe::advance()
   for (int i = 0; i < matter.size(); i++) {
     update_matter(&matter[i]);
   }
+  for (int i = 0; i < forces.size(); i++) {
+
+  }
   //  std::cout << "Position:\n" << matter[0].position.transpose() << "\nVelocity:\n" << matter[0].velocity.transpose() << "\nAcceleration:\n" << matter[0].acceleration.transpose() << "\n\n";
+}
+
+Force calculate_gravity(Matter* source, Matter* target)
+{
+  double distance = sqrt(pow(target->position[0] - source->position[0],2)+pow(target->position[1] - source->position[1],2))+pow(target->position[2] - source->position[2],2);
+  double direction = atan2()
 }
 
 int main()
